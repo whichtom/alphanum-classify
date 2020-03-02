5619022556b978775f3d60b70cf45688c3904e8f
tom <tomcutler39@gmail.com>
2020-03-02 23:00:19 +0000

    low acc, export doesnt work

--- train/model_train.py
+++ train/model_train.py
@@ -8,7 +8,8 @@ from keras.optimizers import SGD
 from keras.preprocessing.image import ImageDataGenerator, img_to_array, load_img
 from keras.callbacks import ModelCheckpoint
 from keras.utils import to_categorical
-import tensorflow.contrib.tensorrt as trt
+from keras import backend as K
+from tensorflow.python.compiler.tensorrt import trt_convert as trt
 from tensorflow.python.framework import graph_io
 from tensorflow.keras.models import load_model
 import numpy as np
@@ -44,84 +45,99 @@ def freeze_graph(graph, session, output, save_pb_dir=save_pb_dir,
         graphdef_frozen = tf.graph_util.convert_variables_to_constants(session,
                                                             graphdef_inf,
                                                             output)
-        graph_io.write_graph(graphdef_frozen, save_pb_dir, save_pb_name
+        graph_io.write_graph(graphdef_frozen, save_pb_dir, save_pb_name,
                             as_text=save_pb_as_text)
         return graphdef_frozen
 
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--file", "-f", type=str, help="file name to save model", default=None, required=True)
-    parser.add_argument("--epochs", "-e", type=int, help="number of epochs", default=12)
-    parser.add_argument("--batch", "-b", type=int, help="batch size", default=32)
-
+    parser.add_argument("--train", action="store_true")
+    parser.add_argument("--export", action="store_true")
     args = parser.parse_args()
 
-    model = Sequential()
-    # input: 28x28 images with 1 channel -> (28, 28, 1) tensors.
-    # this applies 32 convolution filters of size 3x3 each.
-    model.add(Conv2D(32, (3, 3), activation="relu",
-                input_shape=(WIDTH, HEIGHT, 1)))
-    model.add(Conv2D(32, (3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(rate=0.25))
+    if args.train:
+        print("Building model...")
+        model = Sequential()
+        # input: 28x28 images with 1 channel -> (28, 28, 1) tensors.
+        # this applies 32 convolution filters of size 3x3 each.
+        model.add(Conv2D(32, (3, 3), activation="relu",
+                    input_shape=(WIDTH, HEIGHT, 1)))
+        model.add(Conv2D(32, (3, 3), activation="relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(rate=0.25))
 
-    model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(Conv2D(64, (3, 3), activation="relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
-    model.add(Dropout(rate=0.25))
+        model.add(Conv2D(64, (3, 3), activation="relu"))
+        model.add(Conv2D(64, (3, 3), activation="relu"))
+        model.add(MaxPooling2D(pool_size=(2, 2)))
+        model.add(Dropout(rate=0.25))
 
-    model.add(Flatten())
-    model.add(Dense(256, activation="relu"))
-    model.add(Dropout(rate=0.5))
-    model.add(Dense(NUM_CLASSES, activation="softmax"))
+        model.add(Flatten())
+        model.add(Dense(256, activation="relu"))
+        model.add(Dropout(rate=0.5))
+        model.add(Dense(NUM_CLASSES, activation="softmax"))
 
-    model.compile(optimizer="adam", loss="categorical_crossentropy",
-                    metrics=["accuracy"])
-    model.summary()
+        model.compile(optimizer="rmsprop", loss="categorical_crossentropy",
+                        metrics=["accuracy"])
+        model.summary()
 
-    history = model.fit(train_data, train_labels,
-              validation_data=(valid_data, valid_labels),
-              epochs=args.epochs, batch_size=args.batch, verbose=1)
+        print("Fitting model...")
+        history = model.fit(train_data, train_labels,
+                  validation_data=(valid_data, valid_labels),
+                  epochs=12, batch_size=32, verbose=1)
 
-    predictions = [np.argmax(model.predict(np.expand_dims(tensor,
-                                            axis=0))) for tensor in test_data]
-
-
-    test_accuracy = 100*np.sum(np.array(predictions) == np.argmax(test_labels,
-                                                        axis=1))/len(predictions)
-    print("Test accuracy: %.1f%%" % test_accuracy)
-
-    # Save the h5 file
-    model.save(args.file)
-    print("model saved to current working directory as %s", % args.file)
+        predictions = [np.argmax(model.predict(np.expand_dims(tensor,
+                                                axis=0))) for tensor in test_data]
 
 
-    tf.keras.backend.clear_session()
-    tf.keras.backend.set_learning_phase(0)
+        test_accuracy = 100*np.sum(np.array(predictions) == np.argmax(test_labels,
+                                                            axis=1))/len(predictions)
+        print("Test accuracy: %.1f%%" % test_accuracy)
 
-    model = load_model(args.file)
-    session = tf.keras.backend.get_session()
+        # Save the h5 file
+        model.save("model.h5")
+        print("model saved to current working directory")
 
-    input_names = [t.op.name for t in model.inputs]
-    output_names = [t.op.name for t in model.outputs]
+    elif args.export:
+        print("Exporting model...")
+        model = load_model("model.h5")
+        session = K.get_session()
 
-    print(input_names, output_names)
+        input_names = [t.op.name for t in model.inputs]
+        output_names = [t.op.name for t in model.outputs]
 
-    frozen_graph = freeze_graph(session.graph, session,
-                                [out.op.name for out in model.outputs],
-                                save_pb_dir=save_pb_dir)
+        print(input_names, output_names)
 
-    trt_graph = trt.create_inference_graph(
-                    input_graph_def=frozen_graph,
-                    outputs=output_names,
-                    max_batch_size=1,
-                    max_workspace_size_bytes=1<<25,
-                    precision_mode="FP16",
-                    minimum_segment_size=50)
+        frozen_graph = freeze_graph(session.graph, session,
+                                    [out.op.name for out in model.outputs],
+                                    save_pb_dir=save_pb_dir)
+        converter = trt.TrtGraphConverter(
+                                input_graph_def=frozen_graph,
+                                max_batch_size=1,
+                                max_workspace_size_bytes=1<<25,
+                                precision_mode="FP16",
+                                minimum_segment_size=50)
+        trt_graph = converter.convert()
 
-    graph_io.write_graph(trt_graph, ".", "model.pb", as_text=False)
+        graph_io.write_graph(trt_graph, ".", "model2.pb", as_text=False)
 
 
 
+#        with tf.Session() as sess:
+#            with tf.gfile.GFile("model.pb", "rb") as f:
+#                frozen_graph = freeze_graph(session.graph, session,
+#                                    [out.op.name for out in model.outputs],
+#                                    save_pb_dir=save_pb_dir)
+#                frozen_graph.ParseFromString(f.read())
+#            converter = trt.TrtGraphConverter(
+#                                input_graph_def=frozen_graph,
+#                                max_batch_size=1,
+#                                max_workspace_size_bytes=1<<25,
+#                                precision_mode="FP16",
+#                                minimum_segment_size=50)
+#            trt_graph = converter.convert()
+#            sess.run(output_names)
 
+    else:
+        print("Either --train or --export")
