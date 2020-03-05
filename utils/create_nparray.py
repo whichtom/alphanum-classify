a55f7d8079cf0e6a7aca1f23d7bb6e3ffbd96a22
tom <tomcutler39@gmail.com>
2020-03-05 16:01:47 +0000

    fixed label assignment

--- utils/create_nparray.py
+++ utils/create_nparray.py
@@ -7,6 +7,23 @@ import sys
 import cv2
 import numpy as np
 from keras.preprocessing.image import img_to_array
+import re
+import glob
+
+CLASSES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+            "T", "U", "V", "W", "X", "Y", "Z"]
+
+
+def atoi(text):
+    return int(text) if text.isdigit() else text
+
+def natural_keys(text):
+    return [atoi(c) for c in re.split(r'(\d+)', text)]
+
+def listfiles_nohidden(path):
+    return [x for x in os.listdir(path) if not x.startswith(".")]
 
 def load_img_from_folder(folderdir):
     """
@@ -34,13 +51,19 @@ def img_to_nparray(imgdir):
     """
 
     # get number of files in the directory
-    num_files = sum([len(files) for _, _, files in os.walk(imgdir)])
+    if imgdir == "../data/raw/training_data":
+        num_files = 6286
+    elif imgdir == "../data/raw/test_data":
+        num_files = 360
+    elif imgdir == "../data/raw/validation_data":
+        num_files = 545
 
     data = np.zeros([num_files, 28, 28, 1])
-    label = np.zeros([num_files])
+    label = np.empty([num_files], dtype=str)
 
     dirlist = [x[0] for x in os.walk(imgdir)]
     dirlist = dirlist[1:len(dirlist)]
+    dirlist.sort(key=natural_keys) # sorts
     k = 0 # per image 
     p = 0 # per class, index 0-35
 
@@ -51,10 +74,13 @@ def img_to_nparray(imgdir):
             img = img_to_array(img)
             data[k, :, :, :] = img
             data[k, :, :, :] = (data[k, :, :, :]/255.0)
-            label[k] = int(p)
+            label[k] = CLASSES[int(p)]
+            print(label[k])
             k = k+1 # iterates per image in the subdirectory in imgdir
         p = p+1 # iterates per subdirectory in imgdir
 
-    for i in range(len(label)):
-        label[i] = int(label[i])
     return data, label
+
+
+if __name__=="__main__":
+    img_to_nparray("../data/raw/training_data")
