5d31ade12bcbcec0196377660ada9c9a7b90a96f
tom <tomcutler39@gmail.com>
2020-03-06 23:44:09 +0000

    modified train to support csv with better preprocessing

--- train/model_train.py
+++ train/model_train.py
@@ -17,6 +17,7 @@ from tensorflow.keras.models import load_model
 import numpy as np
 import argparse
 from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
+from sklearn.preprocessing import LabelBinarizer
 
 # classes of each output
 classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
@@ -26,24 +27,44 @@ classes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
 
 WIDTH = 28
 HEIGHT = 28
+DEPTH = 20
 NUM_CLASSES = 36
-INPUT_SHAPE = (28,28,1)
-EPOCHS=8
-BATCH_SIZE=32
+INPUT_SHAPE = (WIDTH, HEIGHT, 1)
 
 # training data
-train_data = np.load("../data/nparr/train_data.npy")
-train_labels = np.load("../data/nparr/train_labels.npy")
+train_data = np.loadtxt("../data/csv/train_data.csv", delimiter=",")
+train_labels = np.loadtxt("../data/csv/train_labels.csv", delimiter=",")
 # validation data
-valid_data = np.load("../data/nparr/valid_data.npy")
-valid_labels = np.load("../data/nparr/valid_labels.npy")
+valid_data = np.loadtxt("../data/csv/valid_data.csv", delimiter=",")
+valid_labels = np.loadtxt("../data/csv/valid_labels.csv", delimiter=",")
 # final testing data
-test_data = np.load("../data/nparr/test_data.npy")
-test_labels = np.load("../data/nparr/test_labels.npy")
+test_data = np.loadtxt("../data/csv/test_data.csv", delimiter=",")
+test_labels = np.loadtxt("../data/csv/test_labels.csv", delimiter=",")
 
-train_labels = to_categorical(train_labels)
-valid_labels = to_categorical(valid_labels)
-test_labels = to_categorical(test_labels)
+# updated preprocessing
+
+train_data = np.array([np.reshape(i, (28, 28)) for i in train_data])
+train_data = np.array([i.flatten() for i in train_data])
+
+valid_data = np.array([np.reshape(i, (28, 28)) for i in valid_data])
+valid_data = np.array([i.flatten() for i in valid_data])
+
+test_data = np.array([np.reshape(i, (28, 28)) for i in test_data])
+test_data = np.array([i.flatten() for i in test_data])
+
+label_binarizer = LabelBinarizer()
+train_labels = label_binarizer.fit_transform(train_labels)
+valid_labels = label_binarizer.fit_transform(valid_labels)
+test_labels = label_binarizer.fit_transform(test_labels)
+
+train_data = train_data.astype("float32") / 255
+valid_data = valid_data.astype("float32") / 255
+test_data = test_data.astype("float32") / 255
+
+train_data = train_data.reshape(train_data.shape[0], 28, 28, 1)
+valid_data = valid_data.reshape(valid_data.shape[0], 28, 28, 1)
+test_data = test_data.reshape(test_data.shape[0], 28, 28, 1)
+
 
 save_pb_dir = "."
 
@@ -162,12 +183,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--train", action="store_true")
     parser.add_argument("--export", action="store_true")
+    parser.add_argument("--epoch", "-e", type=int, help="epoch", default=8)
+    parser.add_argument("--batch", "-b", type=int, help="batch size", default=32)
     args = parser.parse_args()
 
     if args.train:
         print("Building model...")
 
-        model = resnet_model(input_shape=INPUT_SHAPE, depth=20,
+        model = resnet_model(input_shape=INPUT_SHAPE, depth=DEPTH,
                             num_classes=NUM_CLASSES)
         model.compile(loss="categorical_crossentropy",
                     optimizer="Adam", metrics=["accuracy"])
@@ -178,7 +201,7 @@ if __name__ == "__main__":
         print("Fitting model...")
         model.fit(train_data, train_labels,
                 validation_data=(valid_data, valid_labels),
-                epochs=EPOCHS, batch_size=BATCH_SIZE, verbose=1,
+                epochs=args.epoch, batch_size=args.batch, verbose=1,
                 shuffle=True)
 
 
