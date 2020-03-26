5cdc97d88c5068e4c458d0320319e28ca22a42a3
tom <tomcutler39@gmail.com>
2020-03-26 18:34:24 +0000

    classify images from loaded model, although not optimizd

---
+++ classify.py
@@ -0,0 +1,44 @@
+import numpy as np
+import keras
+from keras.models import Sequential, load_model
+import cv2
+
+CLASSES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
+            "A", "B", "C", "D", "E", "F", "G", "H", "I",
+            "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
+            "T", "U", "V", "W", "X", "Y", "Z"]
+
+def preprocess_img(img_path):
+    # this is a super janky way to do it and needs to be cleaned up
+    img = cv2.imread(img_path, 0)
+    img = img.flatten()
+    img = img.astype(int)
+    img = np.array([np.reshape(img, (28, 28))])
+    img = np.array([img.flatten()])
+    img = img.astype("float32") / 255
+    img = img.reshape(img.shape[0], 28, 28, 1)
+    return img
+
+def classify(img_path):
+
+    # preprocess the img
+    print("preprocessing image")
+    img = preprocess_img(img_path)
+
+    # predict the class from the img
+    print("predicting class")
+    class_pred_idx = model.predict_classes(img)
+    assert len(class_pred_idx) == 1
+
+    class_pred = CLASSES[class_pred_idx[0]]
+    return class_pred
+
+
+if __name__=="__main__":
+    model = load_model("train/model.h5")
+    class_pred = classify(img_path="data/raw/training_data/1/45.jpg")
+    print(class_pred)
+    class_pred = classify(img_path="data/raw/training_data/2/101.jpg")
+    print(class_pred)
+
+
