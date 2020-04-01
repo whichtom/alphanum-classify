8a3c521347a923f38780229667475dabc64ed7ac
tom <tomcutler39@gmail.com>
2020-04-01 17:18:44 +0100

    argument for classify

--- classify.py
+++ classify.py
@@ -2,6 +2,7 @@ import numpy as np
 import keras
 from keras.models import Sequential, load_model
 import cv2
+import sys
 
 CLASSES = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",
             "A", "B", "C", "D", "E", "F", "G", "H", "I",
@@ -35,10 +36,11 @@ def classify(img_path):
 
 
 if __name__=="__main__":
+    if len(sys.argv) < 2:
+        print("use: $ python3 {} <path/to/image.jpg>".format(sys.argv[0]))
+        exit(-1)
+
     model = load_model("train/model.h5")
-    class_pred = classify(img_path="data/raw/training_data/1/45.jpg")
-    print(class_pred)
-    class_pred = classify(img_path="data/raw/training_data/2/101.jpg")
-    print(class_pred)
 
-
+    class_pred = classify(img_path=sys.argv[1])
+    print("predicted alphanumeric character: {}".format(class_pred))
